3f051de7a29531715d85079b210655d39e19310f
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-11 14:17:37 +0900

    [Feature] - Display the result of Foo

--- js/foo.js
+++ js/foo.js
@@ -1,2 +1,24 @@
 var x = 'foo';
 console.log(window.x);
+
+document.getElementById("show_text").innerHTML = window.x;
+
+// const template = document.createElement("template");
+// template.innerHTML = `
+//     <link href="../css/style.css" rel="stylesheet">
+//     <p></p>
+// `
+
+// class TestIndex extends HTMLElement {
+//     constructor(){
+//         super();
+
+//         this.attachShadow({mode: "open"});
+//         this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+//         this.shadowRoot.querySelector("p").innerText = this.getAttribute(window.x);
+//     }
+// }
+
+
+// window.customElements.define("test-index", TestIndex)
