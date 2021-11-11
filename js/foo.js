4a553328995f6d59f01ec02c91fbe629a3fc3b3a
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-11 14:32:55 +0900

    [Feature] - Update style.css

--- js/foo.js
+++ js/foo.js
@@ -2,23 +2,3 @@ var x = 'foo';
 console.log(window.x);
 
 document.getElementById("show_text").innerHTML = window.x;
-
-// const template = document.createElement("template");
-// template.innerHTML = `
-//     <link href="../css/style.css" rel="stylesheet">
-//     <p></p>
-// `
-
-// class TestIndex extends HTMLElement {
-//     constructor(){
-//         super();
-
-//         this.attachShadow({mode: "open"});
-//         this.shadowRoot.appendChild(template.content.cloneNode(true));
-
-//         this.shadowRoot.querySelector("p").innerText = this.getAttribute(window.x);
-//     }
-// }
-
-
-// window.customElements.define("test-index", TestIndex)
