bd48801d95c563587addbf7794d5e6a802de72ea
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-12 10:17:18 +0900

    [Feature] - Extracted render variable as Promise style

--- js/promise_async_example.js
+++ js/promise_async_example.js
@@ -1,2 +1,32 @@
-const $reuslt = document.querySelector(".result");
-const render = content => {$reuslt.textContent = JSON.stringify(content, null, 2);};
+const $result = document.querySelector(".result");
+const render = content => { $result.textContent = JSON.stringify(content, null, 2);};
+// console.log(render);
+
+const promiseAJAX = (method, url, payload) => {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        console.log(xhr);
+
+        xhr.open(method, url);
+        xhr.setRequestHeader("Content-type", "application/json");
+        xhr.send(JSON.stringify(payload));
+        console.log(JSON.stringify(payload));
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+
+            if (xhr.status >= 200 && xhr.status < 400) {
+                resolve(xhr.response);
+            } else {
+                reject(new Error(xhr.status));
+            }
+        }
+    });
+}
+
+promiseAJAX('GET', 'http://jsonplaceholder.typicode.com/posts/1')
+    .then(JSON.parse)
+    .then(
+        render,
+        console.error
+    );
