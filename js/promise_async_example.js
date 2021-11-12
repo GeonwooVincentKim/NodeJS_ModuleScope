8b312ce8c34e79a989884af275fc609ad03b01d0
GeonwooVincentKim <kdsnop@gmail.com>
2021-11-12 11:12:03 +0900

    [Feature] - Show the result of Promise

--- js/promise_async_example.js
+++ js/promise_async_example.js
@@ -1,6 +1,4 @@
-const $result = document.querySelector(".result");
-const render = content => { $result.textContent = JSON.stringify(content, null, 2);};
-console.log(render);
+
 
 const promiseAJAX = (method, url, payload) => {
     return new Promise((resolve, reject) => {
@@ -13,10 +11,13 @@ const promiseAJAX = (method, url, payload) => {
         console.log(JSON.stringify(payload));
 
         xhr.onreadystatechange = function () {
-            if (xhr.readyState !== XMLHttpRequest.DONE) return;
-
+            if (xhr.readyState !== XMLHttpRequest.DONE) 
+                return;
+            
             if (xhr.status >= 200 && xhr.status < 400) {
                 resolve(xhr.response);
+
+                // xhr.response()
             } else {
                 reject(new Error(xhr.status));
             }
@@ -24,9 +25,45 @@ const promiseAJAX = (method, url, payload) => {
     });
 }
 
-promiseAJAX('GET', 'http://jsonplaceholder.typicode.com/posts/1')
-    .then(JSON.parse)
+const $result = document.querySelector(".result");
+const render = content => {
+    $result.textContent = JSON.stringify(content, null, 2);
+    /*$result.innerHTML = `
+        <div class="result_list">
+            <div class="list_body">
+               ${JSON.stringify(content, null, 2)}
+            </div>
+        </div>
+    `*/
+
+    console.log($result.textContent);
+    // console.log($result.textContent(res).get(1));
+};
+console.log(render);
+
+const url = 'http://jsonplaceholder.typicode.com/posts';
+
+promiseAJAX('GET', `${url}/1`)
     .then(
-        render,
-        console.error
-    );
+        res => promiseAJAX('GET', `${url}?userId=${JSON.parse(res).userId}`)
+        // .foreach(element => {     item = 'User ID : ' + element.userId + '\nID : ' +
+        // element.id + '\nTitle : ' +             element.title + '\nCompleted : ' +
+        // element.completed     console.log(item);     result.append(item + "\n"); })
+    )
+    .then(JSON.parse)
+    .then(render)
+    // .then((res) => {     var item = ''     res.foreach(element => {         item
+    // = 'User ID : ' + element.userId + '\nID : ' + element.id + '\nTitle : ' +
+    // element.title + '\nCompleted : ' + element.completed
+    // console.log(item);         result.append(item + "\n");     })
+    .then((response) => {
+        // var item = ''
+        // response.foreach(element => {
+        //     item = 'User ID : ' + element.userId + '\nID : ' + element.id + '\nTitle : ' +
+        //             element.title + '\nCompleted : ' + element
+        //         .completed
+        //         console
+        //         .log(item);
+        //     result.append(item + "\n");
+        // })
+    }).catch(console.error);
